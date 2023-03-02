98cb040788d51da1c9c7c773d8818e5f28922b7b
Carlos-Gabriel-Rojas <carlosgabrielrojas.dev@gmail.com>
2023-03-02 10:59:56 -0300

    hours, minutes and seconds variables

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 function updateClock() {
     const clockContainer = document.querySelector("#clock")
     const currentTime = new Date()
-    
+    const hours = currentTime.getHours()
+    const minutes = currentTime.getMinutes()
+    const seconds = currentTime.getSeconds()
 }
